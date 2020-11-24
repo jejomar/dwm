28d479c25a7c8807f94f008468619ce4521206d8
Jejomar Cariaga <jejomarcariaga.jc@gmail.com>
2020-11-24 13:02:46 +0800

    added gruvbox and Iosevka Term

--- config.def.h
+++ config.def.h
@@ -5,17 +5,18 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Iosevka Term:size=8" };
+static const char dmenufont[]       = "Iosevka Term:size=8";
+static const char col_light1[]      = "#fbf1c7";
+static const char col_light2[]      = "#d5c4a1";
+static const char col_dark1[]       = "#282828";
+static const char col_dark2[]       = "#665c54";
+static const char col_highlight1[]  = "#d79921";
+static const char col_highlight2[]  = "#504945";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_light1, col_dark1, col_highlight2 },
+	[SchemeSel]  = { col_dark1, col_highlight1, col_highlight1 },
 };
 
 /* tagging */
@@ -56,7 +57,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_dark1, "-nf", col_light1, "-sb", col_highlight1, "-sf", col_dark1, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
